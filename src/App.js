0e1b27b42d6a1e7e7c20f63e98e20285c623877e
mlobeto <mercedeslobeto@gmail.com>
2023-03-26 12:04:30 -0300

    con NavBAr

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import {Home ,
 Landing, 
 detailPost,
 areaContrato }from "./Views";
+
 import { Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar.jsx";
 
 
 function App() {
+  
   return (
     <div className="App">
+     <NavBar/>
       <Route exact path="/" render={()=><Landing />}/> 
       <Route path="/home" render={()=> <Home /> }/>
       <Route path="/detail" component={detailPost} />
--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom"
-import style from "./navBar.module.css"
+import style from "./NavBar.module.css"
 
-const navBar =()=>{
+const NavBar =()=>{
     return(
         <div className={style.mainContainer}>
             <Link to="/detail">Aviso</Link>
+            <Link to="/contrato">Contrato</Link>
+            <Link to="/home">Home</Link>
             
 
         </div>
     )
 }
 
-export default navBar
+export default NavBar
